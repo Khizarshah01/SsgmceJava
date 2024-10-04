4636d2c439710ecb88a2e5342713e0082a86ed56
Khizar Shah <109973520+Khizarshah01@users.noreply.github.com>
2024-10-04 16:35:36 +0530

    Create  PrimeChecker.java

---
+++ PrimeChecker.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class PrimeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        
+        // string me read kara or integer me convert kara 
+        String input = scanner.nextLine();
+        int number = Integer.parseInt(input); // Converting String to int
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+    
+    // Method to check if a number is prime
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false; // 0 and 1 are not prime numbers
+        }
+        for (int i = 2; i <number; i++){
+            if (number % i == 0) {
+                return false; // Found a factor, it's not prime
+            }
+        }
+        return true; // No factors found, it's prime
+    }
+}
