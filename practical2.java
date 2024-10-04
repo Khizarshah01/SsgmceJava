53e08d365a0a260efbdb438cff6c28416eaf5cf2
Khizar Shah <109973520+Khizarshah01@users.noreply.github.com>
2024-10-04 17:25:25 +0530

    Update and rename PrimeChecker.java to practical2.java

--- practical2.java
+++ practical2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrimeChecker {
+public class practical2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
